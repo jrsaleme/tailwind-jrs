c5f5f974effc31b0afcb1458164e06faebc9d274
Josue Saleme <josuesaleme@gmail.com>
2024-04-09 16:11:42 -0300

    fin proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,js}",
-    "./**/*.{html,js}",
-    "./index.html",
-    
+    "./public/**/*.html",
+        
   ],
   theme: {
     extend: {
